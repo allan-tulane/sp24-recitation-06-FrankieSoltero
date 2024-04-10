336228857efe862d2cc336b4fd74200641306c2d
Francisco <fsoltero@tulane.edu>
2024-04-10 12:57:13 -0500

    Done

--- answers.md
+++ answers.md
@@ -1,8 +1,8 @@
 # CMPS 2200 Recitation 06
 ## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:**________Frankie Soltero_________________
+**Name:**________Ivo_________________
 
 
 Place all written answers from `recitation-07.md` here for easier grading.
@@ -10,11 +10,18 @@ Place all written answers from `recitation-07.md` here for easier grading.
 
 
 - **2)**
+W = O(2^n)
 
 - **3)**
+S = O(n)
 
 - **4)**
-
+The count will follow the fibonacci sequence
 - **6)**
-
+Max number of times called: n
+Work: O(n)
+Span: O(n)
 - **8)**
+Max Read: 1
+Work: O(n)
+Span: O(n)
--- main.py
+++ main.py
@@ -4,7 +4,14 @@ def fib_recursive(n, counts):
     counts is a list of n+1 elements, where counts[i] is incremented
     each time fib_recursive(i, counts) is called.
     """    
-    counts[n] += 1
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        counts[n] += 1
+        return fib_recursive(n - 1, counts) + fib_recursive(n - 2, counts)
+    
     ###TODO
     pass
     
@@ -12,11 +19,28 @@ def fib_recursive(n, counts):
     
 def fib_top_down(n, fibs):
     ###TODO
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n in fibs:
+        return fibs[n]
+    else:
+        fibs[n] = fib_top_down(n-1, fibs) + fib_top_down(n-2, fibs)
+        return fibs[n]
     pass
 
 
 def fib_bottom_up(n):
     ###TODO
+    if n <= 1:
+        return n
+    fibSequence = [0] * (n + 1)
+    fibSequence[1] = 1
+
+    for i in range(2, n + 1):
+        fibSequence[i] = fibSequence[i - 1] + fibSequence[i - 2]
+    return fibSequence[n]
     pass
 
 
